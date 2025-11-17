d3fa2b1fa5b8db33620f7b2eb84dd198958e18ca
WebDitto <webditto@proton.me>
2025-11-17 19:31:32 -0300

    fix: Fixed pt/AnimeQ source
    (fix Kohi-den/extensions-source#1116)
    (Kohi-den/extensions-source#1139)
    
    (cherry picked from commit 8a256ff60cbf5f41a651b05a41bff4d6e7506b59)

--- src/pt/animeq/src/eu/kanade/tachiyomi/animeextension/pt/animeq/AnimeQUrlActivity.kt
+++ src/pt/animeq/src/eu/kanade/tachiyomi/animeextension/pt/animeq/AnimeQUrlActivity.kt
@@ -8,7 +8,7 @@ import android.util.Log
 import kotlin.system.exitProcess
 
 /**
- * Springboard that accepts https://animeq.blog/<id> intents
+ * Springboard that accepts https://animeq.net/<id> intents
  * and redirects them to the main Aniyomi process.
  */
 class AnimeQUrlActivity : Activity() {
